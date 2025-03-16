4b25c88e27a874f025b8858fc4f9fb9d9589a4af
James Cohen <james@superfeel.com>
2025-03-16 22:38:20 +0000

    add more png tests

--- packages/jpeg/test/test_png.c
+++ packages/jpeg/test/test_png.c
@@ -43,13 +43,6 @@ void tearDown(void) {
   // Optional: Called after every test
 }
 
-// Utility function to get the path to a fixture
-const char *getFixturePath(const char *filename) {
-  static char path[256];
-  snprintf(path, sizeof(path), "%s%s", FIXTURE_DIR, filename);
-  return path;
-}
-
 void testPngBasicMessage(void) {
   const char *input = "test/fixtures/test.png";
   // Check file exists
@@ -58,9 +51,9 @@ void testPngBasicMessage(void) {
   TEST_ASSERT_NOT_NULL(file);
   fclose(file);
 
-  const char *output = "test/output/output.png";
+  const char *output = "test/output/basic_message.png";
   const char *message = "Hello, Stego!";
-  const char *header = "STEGO";
+  const char *header = "$$";
 
   int embed_result = embed_message_in_png(input, output, header, message);
   TEST_ASSERT_EQUAL(0, embed_result);
@@ -69,11 +62,58 @@ void testPngBasicMessage(void) {
   TEST_ASSERT_EQUAL(0, extract_result);
 }
 
+void testPngCustomHeader(void) {
+  const char *input = "test/fixtures/test.png";
+  // Check file exists
+  printf("Input file: %s\n", input);
+  FILE *file = fopen(input, "rb");
+  TEST_ASSERT_NOT_NULL(file);
+  fclose(file);
+
+  const char *output = "test/output/custom_header.png";
+  const char *message = "This is a different message";
+  const char *header = "CUSTOM";
+
+  int embed_result = embed_message_in_png(input, output, header, message);
+  TEST_ASSERT_EQUAL(0, embed_result);
+
+  int extract_result = extract_message_from_png(output, header);
+  TEST_ASSERT_EQUAL(0, extract_result);
+}
+
+void testTransparentPng(void) {
+  const char *input = "test/fixtures/transparent_test.png";
+  const char *output = "test/output/transparent_test.png";
+  const char *message = "This is a message embedded in a transparent PNG!";
+  const char *header = "$$";
+
+  int embed_result = embed_message_in_png(input, output, header, message);
+  TEST_ASSERT_EQUAL(0, embed_result);
+
+  int extract_result = extract_message_from_png(output, header);
+  TEST_ASSERT_EQUAL(0, extract_result);
+}
+
+void testTooLargeMessage(void) {
+  const char *input = "test/fixtures/tiny_test.png";
+  const char *output = "test/output/too_large_message.png";
+  const char *message =
+      "This is a message that is too large to embed in a PNG file. I have to "
+      "add some more text to make it longer to ensure that the test fails.";
+  const char *header = "$$";
+
+  int embed_result = embed_message_in_png(input, output, header, message);
+  TEST_ASSERT_EQUAL(-1, embed_result);
+}
+
 int main(void) {
   create_output_dir();
   atexit(cleanup);
 
   UNITY_BEGIN();
   RUN_TEST(testPngBasicMessage);
+  RUN_TEST(testPngCustomHeader);
+  RUN_TEST(testTransparentPng);
+  RUN_TEST(testTooLargeMessage);
   return UNITY_END();
 }
