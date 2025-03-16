dc836158ba66a544fbb74b53e7e095a30c6775ac
James Cohen <james@superfeel.com>
2025-03-16 21:29:52 +0000

    add unity test setup

---
+++ packages/jpeg/tests/test_main.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+
+void setUp(void) {
+  // Optional: Called before every test
+}
+
+void tearDown(void) {
+  // Optional: Called after every test
+}
+
+int main(void) {
+  UNITY_BEGIN();
+
+  return UNITY_END();
+}
