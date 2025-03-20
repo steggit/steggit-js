9d9e00c4f3f723370609e2ad7168ce9ad178d3d0
James Cohen <james@superfeel.com>
2025-03-20 00:07:24 +0000

    update tests

--- packages/stego/__tests__/stego.test.ts
+++ packages/stego/__tests__/stego.test.ts
@@ -2,31 +2,32 @@ import path from 'path';
 import { encodeTextPng, decodeTextPng } from '../index';
 
 const getInputPath = (name: string) => {
-  return path.join(__dirname, '..', '..', '__fixtures__', name);
+  return path.join(__dirname, '..', '__fixtures__', name);
 };
 
 describe('stego', () => {
-  it('should throw if message is blank', async () => {
+  it('should throw if missing arguments', async () => {
     const inputPath = getInputPath('test.png');
     const outputPath = getInputPath('output.png');
-    const message = '';
-    expect(() => encodeTextPng(inputPath, outputPath, message)).toThrow('Message is required');
+    const message = 'test';
+    expect(encodeTextPng(inputPath, outputPath, '')).rejects.toThrow('Message is required');
+    expect(encodeTextPng('', outputPath, message)).rejects.toThrow('Input file is required');
+    expect(encodeTextPng(inputPath, '', message)).rejects.toThrow('Output file is required');
   });
   
   it('should throw if input file not found', async () => {
     const inputPath = getInputPath('not-found.png');
     const outputPath = getInputPath('output.png');
     const message = 'Hello, world!';
-    expect(() => encodeTextPng(inputPath, outputPath, message)).toThrow('Input file does not exist');
+    expect(encodeTextPng(inputPath, outputPath, message)).rejects.toThrow('Input file does not exist');
   });
 
   it('should throw if input file is not a PNG image', async () => {
     const inputPath = getInputPath('test.jpg');
     const outputPath = getInputPath('output.png');
     const message = 'Hello, world!';
-    expect(() => encodeTextPng(inputPath, outputPath, message)).toThrow('Input file must be a PNG image');
+    expect(encodeTextPng(inputPath, outputPath, message)).rejects.toThrow('Input file must be a PNG image');
   });
-  
 
   it('should encode and decode text in a PNG image', async () => {
     const inputPath = getInputPath('test.png');
