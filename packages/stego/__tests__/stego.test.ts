934c7b393c52f8ff485d6c25256e4d1529bc3c4f
James Cohen <james@superfeel.com>
2025-03-20 22:27:37 +0000

    add jpeg tests

--- packages/stego/__tests__/stego.test.ts
+++ packages/stego/__tests__/stego.test.ts
@@ -1,38 +1,84 @@
+import fs from 'fs';
 import path from 'path';
-import { encodeTextPng, decodeTextPng } from '../index';
+import { encodeTextPng, encodeTextJpeg } from '../index';
 
 const getInputPath = (name: string) => {
   return path.join(__dirname, '..', '__fixtures__', name);
 };
 
-describe('stego', () => {
+const jpegOutputPath = getInputPath('output.jpg');
+const pngOutputPath = getInputPath('output.png');
+
+beforeAll(() => {
+  if (fs.existsSync(jpegOutputPath)) {
+    fs.unlinkSync(jpegOutputPath);
+  }
+  if (fs.existsSync(pngOutputPath)) {
+    fs.unlinkSync(pngOutputPath);
+  }
+});
+
+afterAll(() => {
+  if (fs.existsSync(jpegOutputPath)) {
+    fs.unlinkSync(jpegOutputPath);
+  }
+  if (fs.existsSync(pngOutputPath)) {
+    fs.unlinkSync(pngOutputPath);
+  }
+});
+
+describe(encodeTextPng.name, () => {
   it('should throw if missing arguments', async () => {
     const inputPath = getInputPath('test.png');
-    const outputPath = getInputPath('output.png');
     const message = 'test';
-    expect(encodeTextPng(inputPath, outputPath, '')).rejects.toThrow('Message is required');
-    expect(encodeTextPng('', outputPath, message)).rejects.toThrow('Input file is required');
-    expect(encodeTextPng(inputPath, '', message)).rejects.toThrow('Output file is required');
+    await expect(encodeTextPng(inputPath, pngOutputPath, '')).rejects.toThrow('Message is required');
+    await expect(encodeTextPng('', pngOutputPath, message)).rejects.toThrow('Input file is required');
+    await expect(encodeTextPng(inputPath, '', message)).rejects.toThrow('Output file is required');
   });
   
   it('should throw if input file not found', async () => {
     const inputPath = getInputPath('not-found.png');
-    const outputPath = getInputPath('output.png');
     const message = 'Hello, world!';
-    expect(encodeTextPng(inputPath, outputPath, message)).rejects.toThrow('Input file does not exist');
+    await expect(encodeTextPng(inputPath, pngOutputPath, message)).rejects.toThrow('Input file does not exist');
   });
 
   it('should throw if input file is not a PNG image', async () => {
     const inputPath = getInputPath('test.jpg');
-    const outputPath = getInputPath('output.png');
     const message = 'Hello, world!';
-    expect(encodeTextPng(inputPath, outputPath, message)).rejects.toThrow('Input file must be a PNG image');
+    await expect(encodeTextPng(inputPath, pngOutputPath, message)).rejects.toThrow('Input file must be a PNG image');
   });
 
   it('should encode and decode text in a PNG image', async () => {
     const inputPath = getInputPath('test.png');
-    const outputPath = getInputPath('output.png');
     const message = 'Hello, world!';
-    expect(encodeTextPng(inputPath, outputPath, message)).resolves.toBeUndefined();
+    await expect(encodeTextPng(inputPath, pngOutputPath, message)).resolves.toBeUndefined();
+  });
+});
+
+describe(encodeTextJpeg.name, () => {
+  it('should throw if missing arguments', async () => {
+    const inputPath = getInputPath('test.jpg');
+    const message = 'test';
+    await expect(encodeTextJpeg(inputPath, jpegOutputPath, '')).rejects.toThrow('Message is required');
+    await expect(encodeTextJpeg('', jpegOutputPath, message)).rejects.toThrow('Input file is required');
+    await expect(encodeTextJpeg(inputPath, '', message)).rejects.toThrow('Output file is required');
+  });
+  
+  it('should throw if input file not found', async () => {
+    const inputPath = getInputPath('not-found.jpg');
+    const message = 'Hello, world!';
+    await expect(encodeTextJpeg(inputPath, jpegOutputPath, message)).rejects.toThrow('Input file does not exist');
+  });
+
+  it('should throw if input file is not a JPEG image', async () => {
+    const inputPath = getInputPath('test.png');
+    const message = 'Hello, world!';
+    await expect(encodeTextJpeg(inputPath, jpegOutputPath, message)).rejects.toThrow('Input file must be a JPEG image');
+  });
+
+  it('should encode and decode text in a JPEG image', async () => {
+    const inputPath = getInputPath('test.jpg');
+    const message = 'Hello, world!';
+    await expect(encodeTextJpeg(inputPath, jpegOutputPath, message)).resolves.toBeUndefined();
   });
 });
