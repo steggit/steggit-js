b794b51b2c8bf6f0f366c8b36cc0cd87c007492f
James Cohen <james@superfeel.com>
2025-03-21 23:40:48 +0000

    add missing exported functions

--- packages/browser/build.sh
+++ packages/browser/build.sh
@@ -9,6 +9,6 @@ emcc \
   -s USE_LIBPNG=1 \
   -s USE_LIBJPEG=1 \
   -s MODULARIZE=1 \
-  -s 'EXPORTED_FUNCTIONS=["_encode_png","_encode_jpeg","_decode_png","_decode_jpeg"]' \
+  -s 'EXPORTED_FUNCTIONS=["_encode_png","_encode_jpeg","_decode_png","_decode_jpeg","_malloc","_free","stringToUTF8","UTF8ToString"]' \
   -s 'EXPORTED_RUNTIME_METHODS=["ccall","cwrap"]' \
   -O3
