25f392a0f7c400a6a6947f97696c3914bd7dd6fc
James Cohen <james@superfeel.com>
2025-03-22 14:55:12 +0000

    add more encode tests

--- packages/browser/__tests__/stego.test.ts
+++ packages/browser/__tests__/stego.test.ts
@@ -32,13 +32,63 @@ describe('encode png - error handling', () => {
     const inputPath = getInputPath('test.png');
     const input = fs.readFileSync(inputPath);
     const message = 'test';
-    await expect(encodeTextPng(input, '')).rejects.toThrow('Error: Message is required');
+    await expect(encodeTextPng(input, '')).rejects.toThrow('No message provided');
     await expect(encodeTextPng('', message)).rejects.toThrow('No input provided');
   });
+
   it('should throw if input is not a PNG image', async () => {
-    const inputPath = getInputPath('test.jpg');
+    const inputJpegPath = getInputPath('test.jpg');
+
+    // Buffer
+    const input = fs.readFileSync(inputJpegPath);
+    await expect(encodeTextPng(input, 'test')).rejects.toThrow('Error: Input file must be a PNG image');
+
+    // File
+    const file = new File([input], 'test.jpg', { type: 'image/jpeg' });
+    await expect(encodeTextPng(file, 'test')).rejects.toThrow('File is not a valid png image');
+
+    // String
+    const inputString = fs.readFileSync(inputJpegPath, 'base64');
+    await expect(encodeTextPng(inputString, 'test')).rejects.toThrow('Input string is not a valid png image');
+  });
+
+  it('should throw if message is too long for image to fit', async () => {
+    const inputPath = getInputPath('tiny_test.png');
     const input = fs.readFileSync(inputPath);
-    await expect(encodeTextPng(input, 'test')).rejects.toThrow('Input is not a PNG image');
+    const message = 'Hello, world!'.repeat(1000);
+    await expect(encodeTextPng(input, message)).rejects.toThrow(/Message too large for image! Available space = \d+ bits, message size = \d+ bits/);
   });
 });
 
+describe('encode jpeg - error handling', () => {
+  it('should throw if missing arguments', async () => {
+    const inputPath = getInputPath('test.jpg');
+    const input = fs.readFileSync(inputPath);
+    const message = 'test';
+    await expect(encodeTextJpeg(input, '')).rejects.toThrow('No message provided');
+    await expect(encodeTextJpeg('', message)).rejects.toThrow('No input provided');
+  });
+
+  it('should throw if input is not a JPEG image', async () => {
+    const inputPngPath = getInputPath('test.png');
+
+    // Buffer
+    const input = fs.readFileSync(inputPngPath);
+    await expect(encodeTextJpeg(input, 'test')).rejects.toThrow('Error: Input file must be a JPEG image');
+
+    // File
+    const file = new File([input], 'test.png', { type: 'image/png' });
+    await expect(encodeTextJpeg(file, 'test')).rejects.toThrow('File is not a valid jpeg image');
+
+    // String
+    const inputString = fs.readFileSync(inputPngPath, 'base64');
+    await expect(encodeTextJpeg(inputString, 'test')).rejects.toThrow('Input string is not a valid jpeg image');
+  });
+
+  it('should throw if message is too long for image to fit', async () => {
+    const inputPath = getInputPath('tiny_test.jpg');
+    const input = fs.readFileSync(inputPath);
+    const message = 'Hello, world!'.repeat(1000);
+    await expect(encodeTextJpeg(input, message)).rejects.toThrow(/Message too large for image! Available space = \d+ bits, message size = \d+ bits/);
+  });
+});
