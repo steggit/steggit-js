4ff8d994d3fd82bfa22975ad8b3ca00e000ebb1b
James Cohen <james@superfeel.com>
2025-03-23 11:59:22 +0000

    add root eslint and tsconfig

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ['dist/**', '.eslintrc.js'],
+  root: true,
+  rules: {
+    'import/prefer-default-export': [
+      'off',
+      {
+        except: ['**/index.ts', '**/index.tsx'],
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'never',
+      {
+        json: 'always',
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'max-len': ['warn', 150],
+    'prettier/prettier': 'warn',
+    curly: ['error', 'all'],
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
