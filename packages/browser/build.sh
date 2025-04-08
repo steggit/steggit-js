55d5e0f67397239b90bbf6966d8f752fc6acf11d
James Cohen <james@superfeel.com>
2025-04-08 09:08:10 +0100

    optimise wasm build

--- packages/browser/build.sh
+++ packages/browser/build.sh
@@ -11,4 +11,6 @@ emcc \
   -s MODULARIZE=1 \
   -s 'EXPORTED_FUNCTIONS=["_encode_png","_encode_jpeg","_decode_png","_decode_jpeg","_malloc","_free","stringToUTF8","UTF8ToString","FS","getValue","setValue"]' \
   -s 'EXPORTED_RUNTIME_METHODS=["ccall","cwrap"]' \
+  -s ENVIRONMENT=web \
+  --closure 1 \
   -O3
